27ee28c3868f23ce20ddfcb11d2615a84224d8f7
Kgor0 <98019162+Kgor0@users.noreply.github.com>
2024-08-29 13:29:14 +0200

    Add files via upload

---
+++ exercises.sql
@@ -0,0 +1,44 @@
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: Employees
+ */
+-- 
+
+
+-- Question 1: What is the average salary for the company?
+-- Table: Salaries
+SELECT AVG(salary) FROM salaries;
+
+-- Question 2: What year was the youngest person born in the company?
+-- Table: employees
+SELECT MAX(birth_date) FROM employees;
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: France
+ */
+-- 
+
+-- Question 1: How many towns are there in france?
+-- Table: Towns
+SELECT COUNT(towns) FROM France;
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: World
+ */
+-- 
+
+-- Question 1: How many official languages are there?
+-- Table: countrylanguage
+
+-- Question 2: What is the average life expectancy in the world?
+-- Table: country
+
+-- Question 3: What is the average population for cities in the netherlands?
+-- Table: city
+
+
